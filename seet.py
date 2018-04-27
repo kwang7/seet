78c81b1da3f82e7e0d22400549828a2f894ed6c9
kwang7 <kwang7@stuy.edu>
2018-04-26 23:03:28 -0400

    :poop:

--- seet.py
+++ seet.py
@@ -1,3 +1,9 @@
+#Kelly Wang Jennifer Zhang
+#SoftDev2 pd7
+#K16 Ready, Set, Math!
+#2018-04-26
+
+
 a = [1,2,3]
 b = [2,3,4]
 
@@ -7,10 +13,17 @@ def intersection(a,b):
 def set_difference(a,b):
     return [x for x in a if x not in b]
 
+def union(a,b):
+    return set_difference(a,b) + set_difference(b,a) + intersection(a,b)
 
+def sym_diff(a,b):
+    return set_difference( union(a,b) , intersection(a,b) )
 
+def cart_prod(a,b):
+    return  [ (x,y) for x in a for y in b ]
+    
 print intersection(a,b)
-li1= set_difference(a,b)
-li2= set_difference(b,a)
-li1.append(li2)
-print li1
+print set_difference(a,b)
+print union(a,b)
+print sym_diff(a,b)
+print cart_prod(a,b)
